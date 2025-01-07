d05435086034310f919386dbc63f8211d92ffabc
Elias Brunnberg <eliazbrunnberg@outlook.com>
2025-01-07 01:27:32 +0100

    changes

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://todo-application-topaz.vercel.app/api"
-    : "http://localhost:5000/api";
+    ? "https://todo-application-topaz.vercel.app/api" // Backend i produktion
+    : "http://localhost:5000/api"; // Backend lokalt
 
 export default API_BASE_URL;
